0d8969d9792d00c0873f569071a3e4272a415d38
DuckyBlender <alan@alanklas.com>
2023-03-01 22:43:14 +0100

    so much things are working wtf

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,5 +2,17 @@
 	import '../app.css';
 </script>
 
-<slot />
+<div class="bg-gradient-to-bl from-blue-800 to-blue-900 min-h-screen">
+	<!-- Center the content -->
+	<div class="flex justify-center items-center h-full p-8">
+		<!-- Set the width of the content -->
+		<div class="w-3/4">
+			<!-- Set the background color and if the content is too high, make it scrollable-->
+			<div class="bg-gradient-to-bl from-slate-600 to-slate-700 rounded-lg p-4 overflow-y-auto">
+				<!-- Set the content -->
+				<slot />
+			</div>
+		</div>
+	</div>
+</div>
 <!-- slot is a placeholder for the content of the page -->
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,7 +4,7 @@
 
 	let message = '';
 	let messages: { name: string; message: string }[] = [];
-	let replying = false;
+	var replying = false;
 
 	// add messages to the array
 	function addMessage(name: string, message: string) {
@@ -14,12 +14,12 @@
 			replying = true;
 			let requestBody = {
 				model: 'gpt-3.5-turbo',
-				messages: [
-					{
-						role: 'user',
-						text: message
-					}
-				]
+				messages: messages.map((msg) => {
+					return {
+						role: msg.name === 'ChatGPT' ? 'assistant' : 'user',
+						content: msg.message
+					};
+				})
 			};
 			fetch('https://api.openai.com/v1/chat/completions', {
 				method: 'POST',
@@ -27,8 +27,6 @@
 					Authorization: 'Bearer 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',
 					'Content-Type': 'application/json'
 				},
-				// body: JSON.stringify({ requestBody })
-				// to fix the model is not found
 
 				body: JSON.stringify(requestBody)
 			})
@@ -39,11 +37,12 @@
 					let response = res.choices[0].message.content;
 					addMessage('ChatGPT', response);
 				});
+			console.log('replying: ' + replying);
 			replying = false;
 		}
 	}
 
-	addMessage('ChatGPT', 'Welcome to ChatGPT!');
+	addMessage('ChatGPT', 'Ask me anything!');
 	// If the user inputs a name, store it in localStorage
 	// so that it can be used in the next session
 	function startChatting() {
@@ -56,45 +55,84 @@
 	}
 </script>
 
-<h1>Welcome to ChatGPT!</h1>
-<p>ChatGPT is a chatbot that uses GPT-3.5 to generate responses to your messages.</p>
-<p>It's still in development, so please be patient with it.</p>
+{#if loggedIn === false}
+	<div class="font-lexend text-center border-b-2 text-white mb-4">
+		<h1>Welcome to ChatGPT!</h1>
+		<p>ChatGPT is a chatbot that uses GPT-3.5 to generate responses to your messages.</p>
+		<p>It's still in development, so please be patient with it.</p>
 
-<p>To start, enter your name and click "Start Chatting".</p>
-<input type="text" bind:value={name} placeholder="Your name" />
-<button on:click={startChatting}>Start Chatting</button>
-
-{#if loggedIn}
-	<br /><br />
+		<p>To start, enter your name and click "Start Chatting".</p>
+	</div>
+	<div class="font-lexend text-center text-black">
+		<input type="text" bind:value={name} placeholder="Your name" class="rounded-md p-2" />
+		<button on:click={startChatting} class="text-white bg-blue-500 rounded-md p-2">
+			<!-- On click start chatting -->
+			Start Chatting
+		</button>
+	</div>
+{:else}
 	<!-- If the user is logged in, show the chat -->
 	{#each messages as msg}
 		{#if msg.name === name}
 			<!-- using tailwind css -->
-			<div class="bg-blue-500 text-white rounded-lg p-2">
+			<div class="bg-blue-700 text-white rounded-lg p-2 my-2">
 				{name}: {msg.message}
 			</div>
 		{:else}
-			<div class="bg-gray-500 text-white rounded-lg p-2">
+			<div class="bg-gray-700 text-white rounded-lg p-2 my-2">
 				ChatGPT: {msg.message}
 			</div>
 		{/if}
 	{/each}
-	<input type="text" bind:value={message} placeholder="Your message" />
+	<input type="text" bind:value={message} placeholder="Your message" class="rounded-md p-2" />
 	{#if replying}
 		<button
 			on:click={() => addMessage(name, message)}
 			disabled
-			class="bg-gray-500 text-white rounded-lg p-2"
+			class="bg-gray-500 text-white rounded-md p-2"
 		>
-			ChatGPT is replying...
+			Replying...
 		</button>
 	{:else}
 		<button
 			on:click={() => addMessage(name, message)}
-			class="bg-blue-500 text-white rounded-lg p-2"
+			class="bg-blue-500 text-white rounded-md p-2"
 		>
 			<!-- On click reset the message -->
 			Reply
 		</button>
 	{/if}
+	<button
+		on:click={() => {
+			messages = [];
+			addMessage('ChatGPT', 'Ask me anything!');
+		}}
+		class="bg-blue-500 text-white rounded-md p-2"
+	>
+		<!-- On click reset the message -->
+		Reset
+	</button>
+	<button
+		on:click={() => {
+			let text = '';
+			messages.forEach((msg) => {
+				text += msg.name + ': ' + msg.message + '\n';
+			});
+			let element = document.createElement('a');
+			element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+			element.setAttribute('download', 'chat.txt');
+
+			element.style.display = 'none';
+			document.body.appendChild(element);
+
+			element.click();
+
+			document.body.removeChild(element);
+			// I have actually no idea how this works, copilot did it for me lol
+		}}
+		class="bg-blue-500 text-white rounded-md p-2"
+	>
+		<!-- On click get all of the messages and save them to a txt file -->
+		Save
+	</button>
 {/if}
