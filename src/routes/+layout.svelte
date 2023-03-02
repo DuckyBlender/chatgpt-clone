71cbc3676f0bb8f65aea1a848113301355f1062e
Alan <alan@alanklas.com>
2023-03-02 16:31:32 +0100

    removed typing in the name

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,12 +2,30 @@
 	import '../app.css';
 </script>
 
-<div class="bg-gradient-to-l from-blue-600 to-blue-900 min-h-screen">
+<div class="bg-gradient-to-l from-blue-700 to-blue-900 min-h-screen -z-10">
 	<!-- Center the content -->
 	<div class="flex justify-center items-center h-full p-8">
 		<!-- Set the width of the content -->
 		<div class="w-11/12">
-			<!-- Set the background color and if the content is too high, make it scrollable-->
+			<!-- Header -->
+			<div
+				class="bg-gradient-to-bl from-slate-600 to-slate-700 rounded-lg p-4 shadow-md mb-4 flex flex-row justify-between"
+			>
+				<div>
+					<!-- Title -->
+					<h1 class="text-2xl text-white font-bold">ChatGPT</h1>
+					<!-- Subtitle -->
+					<p class="text-sm text-gray-300">An advanced AI chatbot powered by GPT-3.5</p>
+				</div>
+				<div class="text-right">
+					<!-- Title -->
+					<h1 class="text-md text-white font-light">Website by: DuckyBlender</h1>
+					<!-- Subtitle -->
+					<p class="text-sm font-light text-gray-300">Version 1.0.0</p>
+				</div>
+			</div>
+
+			<!-- Main content -->
 			<div
 				class="bg-gradient-to-bl from-slate-600 to-slate-700 rounded-lg p-4 overflow-y-auto shadow-md"
 			>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,8 @@
 <script lang="ts">
+	import { onMount } from 'svelte';
 	import { tweened } from 'svelte/motion';
 	import Layout from './+layout.svelte';
-	let name = '';
-	let loggedIn = false;
+	let username = 'Human';
 
 	let message = '';
 	let messages: { name: string; message: string }[] = [];
@@ -11,6 +11,14 @@
 	// Global cooldown bool which can be changed from a function
 	let cooldown = false;
 	// TODO: Add a cooldown to the button
+
+	// Focus the textview
+	onMount(() => {
+		let input = document.getElementById('messageInput');
+		if (input !== null) {
+			input.focus();
+		}
+	});
 
 	function saveMessages() {
 		let text = '';
@@ -35,176 +43,121 @@
 		if (cooldown) return;
 		// buttonCooldown();
 		messages = [...messages, { name, message }];
-		if (name !== 'ChatGPT') {
-			// send the message to the server
-			let requestBody = {
-				model: 'gpt-3.5-turbo',
-				messages: messages.map((msg) => {
-					return {
-						role: msg.name === 'ChatGPT' ? 'assistant' : 'user',
-						content: msg.message
-					};
-				})
-			};
+		if (name === 'ChatGPT') return;
 
-			thinking = true;
-			fetch('https://api.openai.com/v1/chat/completions', {
-				method: 'POST',
-				headers: {
-					Authorization: 'Bearer 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',
-					'Content-Type': 'application/json'
-				},
-
-				body: JSON.stringify(requestBody)
+		// send the message to the server
+		let requestBody = {
+			model: 'gpt-3.5-turbo',
+			messages: messages.map((msg) => {
+				return {
+					role: msg.name === 'ChatGPT' ? 'assistant' : 'user',
+					content: msg.message
+				};
 			})
-				.then((res) => res.json())
-				.then((res) => {
-					// get the response from the server
-					console.log(res);
-					let response = res.choices[0].message.content;
-					addMessage('ChatGPT', response);
-				});
-			// Start a 3 second timer
-			thinking = false;
-		}
+		};
+
+		thinking = true;
+		fetch('https://api.openai.com/v1/chat/completions', {
+			method: 'POST',
+			headers: {
+				// Authorization: 'Bearer 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',
+				Authorization: 'Bearer ' + import.meta.env.VITE_OPENAI_API_KEY,
+				'Content-Type': 'application/json'
+			},
+
+			body: JSON.stringify(requestBody)
+		})
+			.then((res) => res.json())
+			.then((res) => {
+				// get the response from the server
+				console.log(res);
+				let response = res.choices[0].message.content;
+				addMessage('ChatGPT', response);
+			});
+		// Start a 3 second timer
+		thinking = false;
 	}
 
 	addMessage('ChatGPT', 'Ask me anything!');
-	// If the user inputs a name, store it in localStorage
-	// so that it can be used in the next session
-	function startChatting() {
-		if (name !== '') {
-			if (name === 'ChatGPT') {
-				alert('Please enter a name other than ChatGPT');
-				return;
-			}
-			localStorage.setItem('name', name);
-			loggedIn = true;
-		} else {
-			alert('Please enter a name');
-		}
-	}
 </script>
 
-{#if loggedIn === false}
-	<div class="font-lexend text-center border-b-2 text-white mb-4">
-		<h1 class="text-4xl pb-2">Welcome to ChatGPT</h1>
-		<p>ChatGPT is a chatbot that uses GPT-3.5 to generate human-like responses to your messages.</p>
-		<p>To start, enter your name and click "Start Chatting".</p>
-	</div>
-	<div
-		class="font-lexend text-center text-black flex flex-row justify-center items-center space-x-2"
-	>
-		<input
-			type="text"
-			bind:value={name}
-			on:keydown={(e) => {
-				if (e.key === 'Enter') {
-					e.preventDefault();
-					startChatting();
-				}
-			}}
-			placeholder="Your name"
-			class="rounded-md p-2 shadow-md flex-grow"
-		/>
-		<button on:click={startChatting} class="text-white bg-blue-500 rounded-md p-2 shadow-md">
-			<!-- On click start chatting -->
-			Start Chatting
-		</button>
-	</div>
-{:else}
-	<!-- If the user is logged in, show the chat -->
-	{#each messages as msg}
-		{#if msg.name === name}
-			<!-- <div class="bg-blue-700 text-white rounded-lg p-2 my-2"> -->
-			<!-- To fix newlines -->
-			<div class="bg-blue-700 text-white rounded-lg p-2 my-2 whitespace-pre-line shadow-md">
-				{name}: {msg.message}
-			</div>
-		{:else}
-			<div class="bg-gray-700 text-white rounded-lg p-2 my-2 whitespace-pre-line shadow-md">
-				ChatGPT: {msg.message}
-			</div>
-		{/if}
-		{#if thinking}
-			<!-- To make this div work we need to add a new variable called thinking to the script tag with a default value of false -->
-			<div class="bg-gray-800 text-white rounded-lg p-2 my-2 whitespace-pre-line shadow-md">
-				ChatGPT: Thinking...
-			</div>
-		{/if}
-	{/each}
-	<!-- To fix shift+enter functionality in the above input, we need to use a textarea -->
-	<textarea
-		bind:value={message}
-		on:keydown={(e) => {
-			if (e.key === 'Enter' && !e.shiftKey) {
-				e.preventDefault();
-				addMessage(name, message);
-				message = '';
-			}
-		}}
-		placeholder="Your message"
-		class="rounded-md p-2 w-full shadow-md"
-		rows="5"
-		id="messageInput"
-	/>
-	<!-- Focus the textview when the page loads -->
-	<script>
-		document.getElementById('messageInput').focus();
-	</script>
+<!-- If the user is logged in, show the chat -->
+{#each messages as msg}
+	{#if msg.name === username}
+		<!-- <div class="bg-blue-700 text-white rounded-lg p-2 my-2"> -->
+		<!-- To fix newlines -->
+		<div class="bg-blue-700 text-white rounded-lg p-2 my-2 whitespace-pre-line shadow-md">
+			{name}: {msg.message}
+		</div>
+	{:else}
+		<div class="bg-gray-700 text-white rounded-lg p-2 my-2 whitespace-pre-line shadow-md">
+			ChatGPT: {msg.message}
+		</div>
+	{/if}
+	{#if thinking}
+		<!-- To make this div work we need to add a new variable called thinking to the script tag with a default value of false -->
+		<div class="bg-gray-800 text-white rounded-lg p-2 my-2 whitespace-pre-line shadow-md">
+			ChatGPT: Thinking...
+		</div>
+	{/if}
+{/each}
+<!-- To fix shift+enter functionality in the above input, we need to use a textarea -->
+<textarea
+	bind:value={message}
+	on:keydown={(e) => {
+		if (e.key === 'Enter' && !e.shiftKey) {
+			e.preventDefault();
+			addMessage(username, message);
+			message = '';
+		}
+	}}
+	placeholder="Your message"
+	class="rounded-md p-2 w-full shadow-md"
+	rows="5"
+	id="messageInput"
+/>
 
-	<!-- Small reminder that the user can adjust the height by dragging it -->
-	<p class="text-xs text-gray-500">
-		You can adjust the height of the input box by dragging the bottom of it
-	</p>
+<!-- Small reminder that the user can adjust the height by dragging it -->
+<p class="text-xs text-gray-500">
+	You can adjust the height of the input box by dragging the bottom of it
+</p>
 
-	<div class="flex flex-row space-x-2">
-		{#if cooldown === false}
-			<button
-				on:click={() => addMessage(name, message)}
-				class="bg-blue-500 text-white rounded-md p-2 shadow-md flex-grow"
-			>
-				<!-- On click reset the message -->
-				Reply
-			</button>
-		{:else}
-			<button disabled class="bg-blue-500 text-white rounded-md p-2 shadow-md flex-grow">
-				<!-- On click reset the message -->
-				Reply
-			</button>
-		{/if}
-
+<div class="flex flex-row space-x-2">
+	{#if cooldown === false}
 		<button
-			on:click={() => {
-				messages = [];
-				addMessage('ChatGPT', 'Ask me anything!');
-				message = '';
-				// Also reset the textview
-				let input = document.getElementById('messageInput');
-				if (input !== null) {
-					// input.value doesnt work
-					input.innerText = '';
-					input.focus();
-				}
-			}}
-			class="bg-blue-500 text-white rounded-md p-2 shadow-md"
+			on:click={() => addMessage(username, message)}
+			class="bg-blue-500 text-white rounded-md p-2 shadow-md flex-grow"
 		>
 			<!-- On click reset the message -->
-			Reset
+			Reply
 		</button>
-		<button on:click={saveMessages} class="bg-blue-500 text-white rounded-md p-2 shadow-md">
-			<!-- On click get all of the messages and save them to a txt file -->
-			Save
+	{:else}
+		<button disabled class="bg-blue-500 text-white rounded-md p-2 shadow-md flex-grow">
+			<!-- On click reset the message -->
+			Reply
 		</button>
-		<!-- Button for resetting the username -->
-		<button
-			on:click={() => {
-				localStorage.removeItem('name');
-				loggedIn = false;
-			}}
-			class="bg-blue-500 text-white rounded-md p-2 shadow-md"
-		>
-			Reset Username
-		</button>
-	</div>
-{/if}
+	{/if}
+
+	<button
+		on:click={() => {
+			messages = [];
+			addMessage('ChatGPT', 'Ask me anything!');
+			message = '';
+			// Also reset the textview
+			let input = document.getElementById('messageInput');
+			if (input !== null) {
+				// input.value doesnt work
+				input.innerText = '';
+				input.focus();
+			}
+		}}
+		class="bg-blue-500 text-white rounded-md p-2 shadow-md"
+	>
+		<!-- On click reset the message -->
+		Reset
+	</button>
+	<button on:click={saveMessages} class="bg-blue-500 text-white rounded-md p-2 shadow-md">
+		<!-- On click get all of the messages and save them to a txt file -->
+		Save
+	</button>
+</div>
