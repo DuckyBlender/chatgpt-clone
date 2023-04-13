63384ef34dcab0b8664842c160d20ec4f7e9792d
DuckyBlender <alan@alanklas.com>
2023-04-13 18:20:19 +0200

    ugh i give up for now

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -355,13 +355,8 @@ async fn main() -> std::io::Result<()> {
     }
 
     HttpServer::new(move || {
-        // Allow only origin from https://chat.ducky.pics or https://chatgpt.ducky.pics
-        let cors = Cors::default()
-            .allowed_origin("https://chat.ducky.pics")
-            .allowed_methods(vec!["GET", "POST", "OPTIONS"])
-            .allowed_headers(vec![http::header::AUTHORIZATION, http::header::ACCEPT])
-            .allowed_header(http::header::CONTENT_TYPE)
-            .max_age(3600);
+        let cors = Cors::permissive();
+        // TODO: Make this more secure by only allowing the chat.ducky.pics domain
 
         App::new()
             .wrap(cors)
