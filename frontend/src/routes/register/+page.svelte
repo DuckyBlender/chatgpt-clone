33ec88917eb10a804da0bdfc9ce5a4ef45f2822d
DuckyBlender <alan@alanklas.com>
2023-04-13 20:22:44 +0200

    remove one space lmao

--- frontend/src/routes/register/+page.svelte
+++ frontend/src/routes/register/+page.svelte
@@ -135,6 +135,6 @@
 			Register
 		</button>
 		<!-- Go back to login -->
-		<a href="/" class=" text-xs text-blue-400 hover:text-blue-500"> Nevermind, go back to login </a>
+		<a href="/" class=" text-xs text-blue-400 hover:text-blue-500">Nevermind, go back to login </a>
 	</div>
 </div>
